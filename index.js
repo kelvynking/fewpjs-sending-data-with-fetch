8368cdf9f3f44b113998765928f0b3890c00813d
kelvynking <kelvyn.m.king@gmail.com>
2021-11-04 15:08:35 +1100

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your code here
+function submitData(name, email) {
+
+    const url = "http://localhost:3000/users"
+
+    let details = {
+        name,
+        email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(details)
+    }
+
+    return fetch(url, configObj)
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.innerHTML = data["id"]
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+// function submitData( name, email ) {
+//     return fetch( 'http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify( {
+//           name,
+//           email
+//         } )
+//       } )
+//       .then( function ( response ) {
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+//   }
+
